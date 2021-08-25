882dea2d6e37c6fc27ab2a7405e39efd80492b99
EWE07 <ewertonbarretolima07@gmail.com>
2021-08-25 14:41:23 +0000

    update beta

--- test/apis.js
+++ test/apis.js
@@ -1,11 +1,10 @@
-const nampis = require('../index.js');
+const nampis = require('../lib/index');
 
 (async () => {
 	console.log(await nampis.cat());
 	console.log(await nampis.dog());
-	console.log(
-		await nampis.xbox({
-			message: 'oi',
+	console.log(nampis.xbox({
+			message: 'hello',
 			header: 'Hello'
 		})
 	);
