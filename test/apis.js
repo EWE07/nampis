218a16855e075e0a6d8a0d381e29ed0813d3a032
EWE07 <ewertonbarretolima07@gmail.com>
2021-08-25 23:42:42 +0000

    Duck api

--- test/apis.js
+++ test/apis.js
@@ -1,8 +1,10 @@
 const nampis = require('../lib/index');
 
 (async () => {
-	console.log(await nampis.cat("gif"));
+	console.log(await nampis.cat());
 	console.log(await nampis.dog());
-	console.log(await nampis.xbox("oi"));
-	console.log(await nampis.fox())
+	console.log(await nampis.fox());
+	console.log(await nampis.duck());
+
+	console.log(nampis.xbox("oi"));
 })();
