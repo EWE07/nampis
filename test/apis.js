3dfafb1744156fbe01e9d61c748a47dddd7730a7
EWE07 <ewertonbarretolima07@gmail.com>
2021-08-25 13:30:56 +0000

    dev

--- test/apis.js
+++ test/apis.js
@@ -1,11 +1,11 @@
-const nampis = require('../index.js');
+const nampis = require('../lib/index');
 
 (async () => {
 	console.log(await nampis.cat());
 	console.log(await nampis.dog());
 	console.log(
-		await nampis.xbox({
-			message: 'oi',
+		nampis.xbox({
+			message: 'hello',
 			header: 'Hello'
 		})
 	);
